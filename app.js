a005e17a5a5c8d2df968e020051fef1a846ebfdc
Bob <xiaopan.baoxp@alibaba-inc.com>
2015-10-27 15:30:36 +0800

    fisrt commit

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * Canon for Jenkins Javascript Overrides
+ *
+ * Leverages Jenkins-included Prototype.js
+ *
+ * @author Rackspace Web Team
+ */
+document.observe("dom:loaded", function () {
+    // Auto Refresh
+    var autoRefreshSelector = $$('#right-top-nav #right-top-nav div.smallfont');
+    if (autoRefreshSelector.length > 0) {
+        var autoRefreshLink = new Element('span').update(autoRefreshSelector[0].innerHTML);
+        $$('span.jenkins_ver')[0].insert({before:autoRefreshLink});
+    }
+    // Click logo, go home.
+    $$('div.logo')[0].on('click', function(){ location = '/'; });
+});
+
+/*
+document.ready(function(){ 
+    var str="商家技术质量部-持续集成";
+    document.getElementsByClassName("logo").innerHtml=str;
+});
+*/
